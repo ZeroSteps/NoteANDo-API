8ea3cd8ef0734af3c488b00974cc818ab7c57408
YorkIsMine <45906131+YorkIsMine@users.noreply.github.com>
2020-07-26 12:02:15 +0300

    Delete __init__.py

--- project/main/__init__.py
+++
@@ -1,6 +0,0 @@
-from project import db
-from project import Article
-
-article = Article(title="AOI AOI ANOSadORRRAAA", description="akjdakjdkajdkadajdkajdkajdkajd")
-db.session.add(article)
-db.session.commit()
